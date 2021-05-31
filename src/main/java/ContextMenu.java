42072edeb5d651a368fbfb86d8abc86ebbf5dfc5
Sebastian Kocemba <sebastian.kocemba1@gmail.com>
2021-05-31 23:44:55 +0200

    Dodanie wielu parametrow do menu, naprawa bug'a z wyskakujacym menu

--- src/main/java/ContextMenu.java
+++ src/main/java/ContextMenu.java
@@ -8,22 +8,45 @@ public class ContextMenu {
     Boolean SUBMITTED = Boolean.FALSE;
     String IMAGE_NAME;
     Float LAMBDA;
+    int lambdaSet;
     Float E_B;
+    int ebSet;
     Float E_N;
+    int enSet;
     Float A_MAX;
+    int aMaxSet;
+    int SPEED_UP;
+    int speedUPSet;
+    int MAX_ITERATIONS;
+    int maxIterationsSet;
     Form form;
 
     public void showMenu() {
+        if (this.IMAGE_NAME != null){
+            Main.imageFilesList.remove(this.IMAGE_NAME);
+            Main.imageFilesList.add(0, this.IMAGE_NAME);
+        }
+        int lambdaInit = ((lambdaInit = this.lambdaSet) != 0) ? lambdaInit : 300;
+        int ebInit = ((ebInit = this.ebSet) != 0) ? ebInit : 20;
+        int enInit = ((enInit = this.enSet) != 0) ? enInit : 6;
+        int aMaxInit = ((aMaxInit = this.aMaxSet) != 0) ? aMaxInit : 50;
+        int speedUpInit = ((speedUpInit = this.speedUPSet) != 0) ? speedUpInit : 100;
+        int maxIterationsInit = ((maxIterationsInit = this.maxIterationsSet) != 0) ? maxIterationsInit : 50000;
+
         this.form = booster
                 .createForm("Neural Gas Settings")
+//                .addMultipleSelection("Test", Boolean.TRUE, "Test", "test3", "Test5"
+//                ).setID("multipleSelection")
                 .addSelection(
                         "Choose test picture",
                         Main.imageFilesList
                 ).setID("image_selection")
-                .addSlider("Set Lambda", 1, 500, 300, 20, 5).setID("LAMBDA")
-                .addSlider("Set E_B (Will by divided by 100)", 0, 100, 20, 20, 1).setID("E_B")
-                .addSlider("Set E_N (Will by divided by 10000)", 0, 100, 6, 20, 1).setID("E_N")
-                .addSlider("Set A_MAX", 1, 200, 50, 20, 5).setID("A_MAX")
+                .addSlider("Set Lambda", 1, 500, lambdaInit, 50, 5).setID("LAMBDA")
+                .addSlider("Set E_B (Will by divided by 100)", 1, 100, ebInit, 20, 1).setID("E_B")
+                .addSlider("Set E_N (Will by divided by 10000)", 1, 100, enInit, 20, 1).setID("E_N")
+                .addSlider("Set A_MAX", 1, 200, aMaxInit, 20, 5).setID("A_MAX")
+                .addSlider("Set Speed", 1, 200, speedUpInit, 20, 5).setID("SPEED_UP")
+                .addSlider("Set Max Number Of Iterations", 1000, 100000, maxIterationsInit, 10000, 1000).setID("MAX_ITERATIONS")
                 .addButton("Submit", () -> {
                             System.out.println("Close this window");
                         }
@@ -31,10 +54,11 @@ public class ContextMenu {
                 .setChangeListener((element, value, form) -> {
                     if (element.getId().equals("close")) {
                         submit(form);
+                        form.close();
                     }
                 })
                 .andWindow()
-                .setSize(500, 700)
+                .setSize(500, 1000)
                 .setUndecorated()
                 .save()
                 .show();
@@ -44,15 +68,25 @@ public class ContextMenu {
     public void submit(Form form){
         this.SUBMITTED = Boolean.TRUE;
         this.IMAGE_NAME = form.getById("image_selection").asString();
-        this.LAMBDA = form.getById("LAMBDA").asFloat();
-        this.E_B = form.getById("E_B").asFloat()/100;
-        this.E_N = form.getById("E_N").asFloat()/10000;
-        this.A_MAX = form.getById("A_MAX").asFloat();
+        this.lambdaSet = form.getById("LAMBDA").asInt();
+        this.LAMBDA = Float.valueOf(this.lambdaSet);
+        this.ebSet = form.getById("E_B").asInt();
+        this.E_B = Float.valueOf(this.ebSet)/100;
+        this.enSet = form.getById("E_N").asInt();
+        this.E_N = Float.valueOf(this.enSet)/10000;
+        this.aMaxSet = form.getById("A_MAX").asInt();
+        this.A_MAX = Float.valueOf(this.aMaxSet);
+        this.speedUPSet = form.getById("SPEED_UP").asInt();
+        this.SPEED_UP = speedUPSet;
+        this.maxIterationsSet = form.getById("MAX_ITERATIONS").asInt();
+        this.MAX_ITERATIONS = maxIterationsSet;
         // Print all results after submit
         form.getElements().forEach(e -> {
             System.out.println(e.getLabel() + " -> " + e.getValue());
         });
-//        form.close();
+        System.out.println(this.LAMBDA + " " +
+                this.E_B + " "+
+                this.E_N + " " +
+                this.A_MAX);
     }
 }
-
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,18 +21,14 @@ public class Main extends PApplet {
     // BASIC SETTINGS
     public static final int HEIGHT = 800;
     public static final int WIDTH = 800;
-    public static final int BACKGROUND_COLOR = 51;
+    public static final int BACKGROUND_COLOR = 60;
     public static final Boolean DRAW_IMAGE = true; //Drawing input photo
     public static final Boolean DRAW_INPUT_NODES = false;
 
-    // Gas settings
-//    public static final float LAMBDA = 300;
-//    public static final float E_B = 0.2f;
-//    public static final float E_N = 0.006f;
     public static final float ALFA = 0.5f;
     public static final float D = 0.995f;
-//    public static final int A_MAX = 50;
-    public static int SPEED_UP = 200;
+    public int ITERATIONS;
+    public boolean isReallyMousePressed;
     public static List<String> imageFilesList; //This will hold image names form settings()
 
 
@@ -64,7 +60,7 @@ public class Main extends PApplet {
                 }
             }
         };
-        imageFilesList = Arrays.asList(directoryPath.list(jpgFilefilter));
+        imageFilesList = new ArrayList<String>(Arrays.asList(directoryPath.list(jpgFilefilter)));
         System.out.println("List of the jpeg files in the specified directory:");
         for(String fileName : imageFilesList) {
             System.out.println(fileName);
@@ -81,6 +77,7 @@ public class Main extends PApplet {
     }
 
     public void initialize() {
+        this.ITERATIONS = 0;
         neuralGas = new NeuralGas(this);
         neuralGas.initializeNeuralGas();
         inputImage = loadImage(menu.IMAGE_NAME);
@@ -90,18 +87,18 @@ public class Main extends PApplet {
     }
 
     public void draw() {
+        drawBackground(); //background has to be drawn each iteration
+        neuralGasNextStep();
+        handleMouse();
+    }
+
+    public void drawBackground(){
+        //Based on DRAW_IMAGE variable either Image or background (black) i being drawn
+        //If DRAW_INPUT_NODES == true calculated input nodes are being drawn
         if (DRAW_IMAGE)
             image(inputImage, 0, 0);
         else
             background(BACKGROUND_COLOR);
-
-        if (this.dataNodes.size() > 0) {
-            for (int i = 0; i < SPEED_UP; i++) {
-                this.neuralGas.nextIteration(this.dataNodes);
-            }
-        }
-
-        this.neuralGas.drawGas();
 
         if (DRAW_INPUT_NODES)
             this.dataNodes.forEach(node -> {
@@ -109,8 +106,19 @@ public class Main extends PApplet {
                 strokeWeight(3f);
                 point(node.x, node.y);
             });
+    }
 
-        handleMouse();
+
+    public void neuralGasNextStep(){
+        //SPEED_UP variable determines how many nodes calculations are skipped before drawing them.
+        if (this.dataNodes.size() > 0){
+            for (int i = 0; (i < menu.SPEED_UP) && (this.ITERATIONS <= menu.MAX_ITERATIONS); i++) {
+                this.ITERATIONS = this.ITERATIONS + 1;
+                this.neuralGas.nextIteration(this.dataNodes);
+            }
+        }
+        System.out.println(this.ITERATIONS);
+        this.neuralGas.drawGas();
     }
 
     public List<PVector> initializeData(PImage image) {
@@ -121,7 +129,7 @@ public class Main extends PApplet {
                 for (int x = 0; x < image.width; x++) {
                     int pos = y * image.width + x;
                     float val = brightness(image.pixels[pos]);
-                    if (val > 240.0f) { //Color treshold. If >240 looks for blacks, if ==0 gets only white
+                    if (val > 240.0f) { //Color threshold. If >240 looks for blacks, if ==0 gets only white
                         list.add(new PVector(x, y));
                     }
                 }
@@ -131,12 +139,15 @@ public class Main extends PApplet {
     }
 
     public void handleMouse(){
+
         if(mousePressed && mouseButton == RIGHT) {
             menu.showMenu();
             if (menu.SUBMITTED == Boolean.TRUE){
                 initialize();
                 menu.SUBMITTED = Boolean.FALSE;
             }
+            mousePressed = false;
+            mouseButton = 0;
         }
     }
 
